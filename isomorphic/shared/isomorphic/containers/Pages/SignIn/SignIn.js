9c95db1fbffff2ea5ad114abf770c116add1f6b3
phatdongto <phatdongto@gmail.com>
2021-08-28 12:23:44 +0700

    add customize

---
+++ isomorphic/shared/isomorphic/containers/Pages/SignIn/SignIn.js
@@ -0,0 +1,156 @@
+import React from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { Link, Redirect, useHistory, useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Input from "@iso/components/uielements/input";
+import Checkbox from "@iso/components/uielements/checkbox";
+import Button from "@iso/components/uielements/button";
+import IntlMessages from "@iso/components/utility/intlMessages";
+import FirebaseLoginForm from "../../FirebaseForm/FirebaseForm";
+import authAction from "@iso/redux/auth/actions";
+import appAction from "@iso/redux/app/actions";
+import Auth0 from "../../Authentication/Auth0/Auth0";
+import useToken from "../../../../../packages/isomorphic/src/useToken";
+import {
+  signInWithGoogle,
+  signInWithFacebook,
+  signInWithGithub,
+  signInWithTwitter,
+} from "@iso/lib/firebase/firebase.authentication.util";
+import SignInStyleWrapper from "./SignIn.styles";
+
+const { login } = authAction;
+const { clearMenu } = appAction;
+async function loginUser(username, password) {
+  const url =
+    "http://econail.localhost/api/login?username=" +
+    username +
+    "&password=" +
+    password;
+  return axios.get(url).then((res) => res.data.data.access_token);
+}
+export default function SignIn({ setToken }) {
+  let history = useHistory();
+  let location = useLocation();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.Auth.idToken);
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [redirectToReferrer, setRedirectToReferrer] = React.useState(false);
+  React.useEffect(() => {}, [isLoggedIn]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser(username, password);
+    console.log(token);
+
+    localStorage.setItem("token", token);
+    history.push("/dashboard");
+  };
+  function handleLogin(e, token = false) {
+    e.preventDefault();
+    if (token) {
+      dispatch(login(token));
+    } else {
+      dispatch(login());
+    }
+    dispatch(clearMenu());
+    history.push("/dashboard");
+  }
+  let { from } = location.state || { from: { pathname: "/dashboard" } };
+
+  if (redirectToReferrer) {
+    return <Redirect to={from} />;
+  }
+  return (
+    <SignInStyleWrapper className="isoSignInPage">
+      <div className="isoLoginContentWrapper">
+        <div className="isoLoginContent">
+          <div className="isoLogoWrapper">
+            <Link to="/dashboard">
+              <IntlMessages id="page.signInTitle" />
+            </Link>
+          </div>
+          <div className="isoSignInForm">
+            <form>
+              <div className="isoInputWrapper">
+                <Input
+                  size="large"
+                  placeholder="Username"
+                  autoComplete="true"
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+              </div>
+
+              <div className="isoInputWrapper">
+                <Input
+                  size="large"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="false"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+
+              <div className="isoInputWrapper isoLeftRightComponent">
+                <Checkbox>
+                  <IntlMessages id="page.signInRememberMe" />
+                </Checkbox>
+                <Button type="primary" onClick={handleSubmit}>
+                  <IntlMessages id="page.signInButton" />
+                </Button>
+              </div>
+
+              <p className="isoHelperText">
+                <IntlMessages id="page.signInPreview" />
+              </p>
+            </form>
+            <div className="isoInputWrapper isoOtherLogin">
+              <Button
+                onClick={signInWithFacebook}
+                type="primary"
+                className="btnFacebook"
+              >
+                <IntlMessages id="page.signInFacebook" />
+              </Button>
+              <Button
+                onClick={signInWithGoogle}
+                type="primary"
+                className="btnGooglePlus"
+              >
+                <IntlMessages id="page.signInGooglePlus" />
+              </Button>
+
+              <Button
+                onClick={() => {
+                  Auth0.login();
+                }}
+                type="primary"
+                className="btnAuthZero"
+              >
+                <IntlMessages id="page.signInAuth0" />
+              </Button>
+
+              <FirebaseLoginForm
+                history={history}
+                login={(token) => dispatch(login(token))}
+              />
+            </div>
+            <div className="isoCenterComponent isoHelperWrapper">
+              <Link to="/forgotpassword" className="isoForgotPass">
+                <IntlMessages id="page.signInForgotPass" />
+              </Link>
+              <Link to="/signup">
+                <IntlMessages id="page.signInCreateAccount" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </SignInStyleWrapper>
+  );
+}
+SignIn.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
